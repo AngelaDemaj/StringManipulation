9ad31eb10ddf4f7998ecf49c9ee5babfdd8655dc
AngelaDemaj <aggelademaj@hotmail.com>
2021-04-21 00:23:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace StringManipulation
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            var text = "Takis";
+            Console.WriteLine(ReverseAndCapitalizeText(text));
+        }
+        public static string ReverseAndCapitalizeText(string text)
+        {
+            var builder = new StringBuilder(string.Join("", text.Reverse()));
+
+            builder[0] = Char.ToUpper(builder[0]);
+
+            builder[builder.Length - 1] = Char.ToLower(builder[builder.Length - 1]);
+
+            return builder.ToString();
+        }
+    }
+}
